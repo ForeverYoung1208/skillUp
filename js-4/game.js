20a46815dc8b72e6b9d005c8846aa1e5bc1922e4
Igor <siafin2010@gmail.com>
2020-03-26 22:46:25 +0200

    a

--- js-4/game.js
+++ js-4/game.js
@@ -1,15 +1,16 @@
 'use strict'
 
+
 class TilesGame {
   constructor(gameOptions){
     const {sizeX, sizeY, cellsX, cellsY, colors, winPattern} = gameOptions;
-    this.border = 0
-    this.cellWidth = sizeX/cellsX - (cellsX-1)*this.border
-    this.cellHight = sizeY/cellsY - (cellsY-1)*this.border
+    this.cellWidth = sizeX/cellsX
+    this.cellHight = sizeY/cellsY
     
     this.cells = []
     this.node = document.createElement('div')
-    this.node.addEventListener('click',this.clickHandler.bind(this))
+    // this.node.addEventListener('click',this.clickHandler.bind(this))
+    this.node.onclick = this.clickHandler.bind(this);
     this.fillCellsRandom(cellsX, cellsY, colors);
 
     this.render = ()=>{
@@ -27,29 +28,43 @@ class TilesGame {
       for (let x = 0; x<cellsX; x++){
         randomPoolIndex = Math.round( Math.random()*colorsPool.length)-1;
         let randomColor = colorsPool.splice(randomPoolIndex,1);
-        this.cells[y][x]= new Cell(randomColor[0], x,y, this.cellWidth, this.cellHight, this.border, this.node);
+        this.cells[y][x]= new Cell(randomColor[0], x,y, this.cellWidth, this.cellHight, this.node);
       }
     }
   }
   
   clickHandler(e){
-    this.moveCell(+e.target.dataset.x, +e.target.dataset.y )
+    this.moveCell(+e.target.dataset.x, +e.target.dataset.y)
   }
 
   moveCell(x,y){
     if ( !isFinite(x) || !isFinite(y) ) return console.log('strange NaN - maybe, misclick');
     if ( this.cells[y][x+1] && this.cells[y][x+1].color==='empty' ){
-      // console.log('right');
+      console.log('right');
 
     }else if( this.cells[y+1] && this.cells[y+1][x].color==='empty' ){
-      // console.log('down');
-      this.cells[y][x].animateMoveDown()
+      
+      console.log('down');
+
+      this.node.onclick=null;
+      this.cells[y][x].moveDown()
+      setTimeout(()=>{
+        this.node.onclick = this.clickHandler.bind(this);
+      }, 1000)
+        
+
+
+      
+      let tmp = this.cells[y][x]
+      this.cells[y][x] = this.cells[y+1][x]
+      this.cells[y+1][x] = tmp
+
 
     }else if( this.cells[y][x-1] && this.cells[y][x-1].color==='empty' ){
-      // console.log('left');
+      console.log('left');
 
     }else if( this.cells[y-1] && this.cells[y-1][x].color==='empty' ){
-      // console.log('up');
+      console.log('up');
 
     }
   }
@@ -58,10 +73,10 @@ class TilesGame {
 }
 
 class Cell{
-  constructor(cellColor, x, y, cellWidth, cellHight, border, gameField ){
+  constructor(cellColor, x, y, cellWidth, cellHight, gameField ){
 
-    const cellOffsetX = cellWidth + border*1
-    const cellOffsetY = cellHight + border*1
+    this.cellWidth = cellWidth
+    this.cellHight = cellHight
 
     this.color = cellColor
 
@@ -71,15 +86,17 @@ class Cell{
 
     this.node.className = 'cell color-'+cellColor;
     
-    this.node.style.width = cellWidth+'px'
     this.node.style.height = cellHight+'px'
-    this.node.style.top = cellOffsetY*y+'px'
-    this.node.style.left = cellOffsetX*x+'px'
+    this.node.style.width = cellWidth+'px'
+    this.node.style.top = cellHight*y+'px'
+    this.node.style.left = cellWidth*x+'px'
     gameField.appendChild(this.node)
   }
 
-  animateMoveDown(startX,startY){
-    this.node.style.top=this.cellOffetY
+  moveDown(){
+
+    this.node.style.top = String(this.node.dataset.y*this.cellHight + this.cellHight) + 'px'
+    
   }
 
 }
