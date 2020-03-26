0daec2c1c321c71bc56d99b825507467e5d131ce
Igor <siafin2010@gmail.com>
2020-03-26 23:43:37 +0200

    a

--- js-4/game.js
+++ js-4/game.js
@@ -41,74 +41,107 @@ class TilesGame {
     if ( !isFinite(x) || !isFinite(y) ) return console.log('strange NaN - maybe, misclick');
     if ( this.cells[y][x+1] && this.cells[y][x+1].color==='empty' ){
       console.log('right');
+      this.cells[y][x].animateRight()
+      this.swapCells({y, x}, {y, x:x+1})             //includes timeout for finish animation
 
     }else if( this.cells[y+1] && this.cells[y+1][x].color==='empty' ){
-      
       console.log('down');      
-      this.node.onclick=null;
-
-      this.cells[y][x].moveDown()
-      
-      let tmp = this.cells[y][x]
-      this.cells[y][x] = this.cells[y+1][x]
-      this.cells[y+1][x] = tmp
-      
-      setTimeout(()=>{
-        this.node.onclick = this.clickHandler.bind(this);
-
-        this.cells[y][x].node.remove();
-        this.cells[y+1][x].node.remove();
-
-        let c1 = this.cells[y][x].color
-        let c2 = this.cells[y+1][x].color
-
-        this.cells[y][x] = new Cell(c1, y, x, this.cellWidth, this.cellHight, this.node)
-        this.cells[y+1][x] = new Cell(c2, y+1, x, this.cellWidth, this.cellHight, this.node)
-
-      }, 1000)
+      this.cells[y][x].animateDown()
+      this.swapCells({y, x}, {y: y+1, x})             //includes timeout for finish animation
       
     }else if( this.cells[y][x-1] && this.cells[y][x-1].color==='empty' ){
       console.log('left');
+      this.cells[y][x].animateLeft()
+      this.swapCells({y, x}, {y, x:x-1})             //includes timeout for finish animation
 
     }else if( this.cells[y-1] && this.cells[y-1][x].color==='empty' ){
       console.log('up');
+      this.cells[y][x].animateUp()
+      this.swapCells({y, x}, {y: y-1, x})             //includes timeout for finish animation
 
     }
   }
 
-  // swapCells(y1,x1, y2,x2)
+  swapCells(c1, c2){
+    // disable click handling to prevent simulatinous moves
+    this.node.onclick=null;
 
+    //swap in cells array
+    let tmp = this.cells[c1.y][c1.x]
+    this.cells[c1.y][c1.x] = this.cells[c2.y][c2.x]
+    this.cells[c2.y][c2.x] = tmp
 
+    // swap in DOM after animation ends through deleting nodes
+    // and then rebuild them from cells array
+    
+    setTimeout(()=>{
+      
+      this.cells[c1.y][c1.x].removeNode();
+      this.cells[c2.y][c2.x].removeNode();
+      
+      this.cells[c1.y][c1.x].buildNode(c1.y, c1.x, this.node);
+      this.cells[c2.y][c2.x].buildNode(c2.y, c2.x, this.node);
+      
+      //re-bind click handler to enable new turns
+      this.node.onclick = this.clickHandler.bind(this);
+    }, 150)    
+
+  }
 }
 
+
+
+
+
 class Cell{
-  constructor(cellColor, y, x,  cellWidth, cellHight, gameField ){
+  constructor(cellColor, y, x,  cellWidth, cellHight, gameFieldNode ){
 
     this.cellWidth = cellWidth
     this.cellHight = cellHight
-
     this.color = cellColor
+    this.buildNode(y,x,gameFieldNode)
+  }
 
+  removeNode(){
+    this.node.remove();
+  }
+
+  buildNode(y,x,gameFieldNode){
     this.node = document.createElement('div')
     this.node.dataset.y = y
     this.node.dataset.x = x
 
-    this.node.className = 'cell color-'+cellColor;
+    this.node.className = 'cell color-'+this.color;
     
-    this.node.style.height = cellHight+'px'
-    this.node.style.width = cellWidth+'px'
-    this.node.style.top = cellHight*y+'px'
-    this.node.style.left = cellWidth*x+'px'
-    gameField.appendChild(this.node)
+    this.node.style.height = this.cellHight+'px'
+    this.node.style.width = this.cellWidth+'px'
+    this.node.style.top = this.cellHight*y+'px'
+    this.node.style.left = this.cellWidth*x+'px'
+    gameFieldNode.appendChild(this.node)
   }
 
-  moveDown(){
-
+  animateDown(){
     this.node.style.top = String(this.node.dataset.y*this.cellHight + this.cellHight) + 'px'
-    
   }
+
+  animateUp(){
+    this.node.style.top = String(this.node.dataset.y*this.cellHight - this.cellHight) + 'px'
+  }
+
+  animateLeft(){
+    this.node.style.left = String(this.node.dataset.x*this.cellWidth - this.cellWidth) + 'px'
+  }
+
+  animateRight(){
+    this.node.style.left = String(this.node.dataset.x*this.cellWidth + this.cellWidth) + 'px'
+  }
+
+
 
 }
+
+
+
 
 const gameOptions1 = {
   sizeX:400,
